fcb074706e5bcfce7b7e234107bc98e30e1faba3
Lhogeshwaran Purushothaman <lhogeshwaran@yahoo.com>
2021-04-19 01:36:04 +1000

    Dag and bash script to move dag to dag_home and test completed.

---
+++ kpmg_dag.py
@@ -0,0 +1,29 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.utils.dates import days_ago
+from datetime import timedelta
+
+default_args = {
+    'owner': 'lhogeshwaran',
+    'depends_on_past': False,
+    'email': ['lhogeshwaran@gmail.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5)
+}
+
+dag = DAG(
+    'kpmg',
+    default_args=default_args,
+    description='An oversimplified replica of project for UTS PQU',
+    schedule_interval=timedelta(days=1),
+    start_date=days_ago(0),
+    tags=['example']
+)
+
+t1 = BashOperator(
+    task_id='prepare_csvs',
+    bash_command="/Users/loki/UTS_MDSI/code_practices/loki_kpmg/data_check.sh",
+    dag=dag
+)
---
+++ reflect_dag.sh
@@ -0,0 +1,6 @@
+#!/use/bin
+
+cp kpmg_dag.py /Users/loki/airflow/dags
+echo $"Copy completed"
+python ~/airflow/dags/kpmg_dag.py
+echo "Test completed"
