5a716f389f563813ef2f03411194e2f1ff8735f9
Lhogeshwaran Purushothaman <lhogeshwaran@yahoo.com>
2021-04-18 23:59:41 +1000

    Bash script updated with for loops.

--- data_check.sh
+++ data_check.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
+for file in data/SFS_Res*
+do
+    in2csv "$file" > "data/temp.csv"
+    csvcut -c "RESP_ID","RESP_SURVEY_ID","RESP_Q_ID","RESP_POINTS","RESP_COMMENT","RESP_FORM_KEY" "data/temp.csv" > "data/SFS_Responses.csv"
+    rm "data/temp.csv"
+done
 
-in2csv "data/SFS_Responses_Export_File_2017-1_to_2017-2(wo CIC TDI).xlsx" > "data/SFS_Responses_Export_File_2017-1_to_2017-2(wo CIC TDI).csv"
-csvcut -c "RESP_ID","RESP_SURVEY_ID","RESP_Q_ID","RESP_POINTS","RESP_COMMENT","RESP_FORM_KEY" "data/SFS_Responses_Export_File_2017-1_to_2017-2(wo CIC TDI).csv" > "data/SFS_Responses_2017.csv"
-rm "data/SFS_Responses_Export_File_2017-1_to_2017-2(wo CIC TDI).csv"
-
-in2csv "data/SFS_Surveys_Export_File_2017-1_to_2017-2.xlsx" > "data/SFS_Surveys_Export_File_2017-1_to_2017-2.csv"
-csvcut -c "SUR_ID","SUR_YEAR","SUR_HALFYEAR","SUR_SUBJECT_CODE","SUR_SUBJECT_ID","SUR_SUBJECT_NAME","SUR_FACULTY_ID","SUR_FACULTY_NAME","SUR_ACADUNIT_ID","SUR_ACADUNIT_NAME","SUR_STUDENT_COUNT" "data/SFS_Surveys_Export_File_2017-1_to_2017-2.csv" > "data/SFS_Surveys_2017.csv"
-rm "data/SFS_Surveys_Export_File_2017-1_to_2017-2.csv"
+for file in data/SFS_Sur*
+do
+    in2csv $file > "data/temp.csv"
+    csvcut -c "SUR_ID","SUR_YEAR","SUR_HALFYEAR","SUR_SUBJECT_CODE","SUR_SUBJECT_ID","SUR_SUBJECT_NAME","SUR_FACULTY_ID","SUR_FACULTY_NAME","SUR_ACADUNIT_ID","SUR_ACADUNIT_NAME","SUR_STUDENT_COUNT" "data/temp.csv" > "data/SFS_Surveys.csv"
+    rm "data/temp.csv"
+done
